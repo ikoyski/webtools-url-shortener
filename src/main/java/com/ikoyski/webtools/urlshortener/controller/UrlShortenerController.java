35c4656da23fcfa0c08a19422554fd09264379f1
ikoyski <christian_sanpedro@yahoo.com>
2025-02-28 17:29:34 +0000

    updated request mappings

--- src/main/java/com/ikoyski/webtools/urlshortener/controller/UrlShortenerController.java
+++ src/main/java/com/ikoyski/webtools/urlshortener/controller/UrlShortenerController.java
@@ -3,6 +3,7 @@ package com.ikoyski.webtools.urlshortener.controller;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ikoyski.webtools.urlshortener.dto.UrlShortenerRequest;
@@ -10,6 +11,7 @@ import com.ikoyski.webtools.urlshortener.dto.UrlShortenerResponse;
 import com.ikoyski.webtools.urlshortener.service.UrlShortenerService;
 
 @RestController
+@RequestMapping("/url-shortener")
 public class UrlShortenerController {
 
 	private UrlShortenerService urlShortenerService;
@@ -18,8 +20,8 @@ public class UrlShortenerController {
 		this.urlShortenerService = urlShortenerService;
 	}
 
-	@PostMapping(path = "api/v1/create")
-	@Cacheable(value = "shortenedUrl", key = "#urlShortenerRequest.url")
+	@PostMapping(path = "/v1")
+	@Cacheable(value = "url-shortener", key = "#urlShortenerRequest.url")
 	public UrlShortenerResponse createShortenedUrl(@RequestBody UrlShortenerRequest urlShortenerRequest) {
 		return urlShortenerService.createShortenedUrl(urlShortenerRequest);
 	}
